83242633b3e89c4558349ddc25673c8aad0fe4e1
eduardagoulart <goularteduarda.a@gmail.com>
2018-12-26 17:40:59 -0200

    inscricao

---
+++ eventex/subscriptions/tests_.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
+    def test_get(self):
+        """Get /inscricao/ must return status code 200"""
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        """Must use subscriptions/subsciption_form.html"""
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
